35b307d1d04cef120ed7fead211f1d82fc8d704f
Thoams Freeborough <thomas@hotsnapper.com>
2016-07-27 16:13:25 +0100

    Check for a dev environment and load dev tools

--- src/app/application_wrapper/App.js
+++ src/app/application_wrapper/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import DevTools from '../../_dev/DevTools';
+
+const environment = process.env.NODE_ENV;
 
 /**
  * @author "Tom Freeborough"  <thomas@hotsnapper.com> (27 Jul 2016)
  *
  * This does not need changing, the idea is that all routes get rendered inside this dumb component and its for
- * them to handle changes and updates. 
+ * them to handle changes and updates. If we are inside of a development environment then we will
+ * want to ensure we enable redux devtools to manage and view changes to the state in our
+ * application.
  */
 const App = (props) => {
   return (
     <div>
       {props.children}
+      {environment === 'development'
+        ?
+        <DevTools />
+        :
+        false
+      }
     </div>
   )
 };
