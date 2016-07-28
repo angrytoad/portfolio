b10b2ed59d35e73dcebd3f95abb3fbdfefa71903
Thoams Freeborough <thomas@hotsnapper.com>
2016-07-28 15:40:46 +0100

    Updated image rendering
    Updated the way images are rendered to fit both a production and
    development environment.

--- src/app/route_components/home/Home.js
+++ src/app/route_components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css';
 
-import logo from './logo.png';
+import './logo.png';
 
 import ExampleContainer from '../../../_examples/example_container';
 
@@ -11,7 +11,7 @@ const Home = (props) => {
       <h1>Application successfully running</h1>
       <div id="logo-wrap">
         <h1>Next generation applications brought to you by</h1>
-        <img src={logo} id="logo" />
+        <img src='/assets/logo.png' id="logo" />
       </div>
 
       <ExampleContainer />
