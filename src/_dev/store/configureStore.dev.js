ef81e00c98ecc09af81326530c83dcd0c7cbf78f
Thoams Freeborough <thomas@hotsnapper.com>
2016-07-28 15:39:31 +0100

    Remove DebugSession
    Debug session unfortunately does not play well with server side
    rendering and even though it isn't used babel still needs to scan the
    file and due to the non-existent window object this fails.

--- src/_dev/store/configureStore.dev.js
+++ src/_dev/store/configureStore.dev.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { persistState } from 'redux-devtools';
 
 /**
  * @author "Tom Freeborough"  <thomas@hotsnapper.com> (27 Jul 2016)
@@ -23,17 +22,8 @@ const enhancer = compose(
   // Middleware you want to use in development:
   applyMiddleware(thunkMiddleware),
   // Required! Enable Redux DevTools with the monitors you chose
-  DevTools.instrument(),
-  // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
-  persistState(getDebugSessionKey())
+  DevTools.instrument()
 );
-
-function getDebugSessionKey() {
-  // You can write custom logic here!
-  // By default we try to read the key from ?debug_session=<key> in the address bar
-  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-  return (matches && matches.length > 0)? matches[1] : null;
-}
 
 export default function configureStore(initialState) {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
