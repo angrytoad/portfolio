bbea9a278819cf0c004341234cbefbe687778acc
Thoams Freeborough <thomas@hotsnapper.com>
2016-07-28 15:38:40 +0100

    Fixed asset rendering in development mode

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -67,7 +67,7 @@ module.exports = {
       },
       {
         test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
-        loader: 'file',
+        loader: 'file?name=assets/[name].[ext]',
       },
       {
         test: /\.(mp4|webm)$/,
