469c6c35f9817c904fe973f97e5587f979119a0a
Thoams Freeborough <thomas@hotsnapper.com>
2016-07-29 10:22:57 +0100

    More example actions and comments

--- src/_examples/example_actions.js
+++ src/_examples/example_actions.js
@@ -1,3 +1,20 @@
-
+/**
+ * @author "Tom Freeborough"  <thomas@hotsnapper.com> (29 Jul 2016)
+ *
+ * This is an example of how a simple synchronous action is carried out as a general rule all actions must be
+ * dispatched with a type property so you can identify what needs to happen to the states you are
+ * changing. Sometimes if you wish to pass data you may do so by including the data
+ * property which should be an object of the data you wish to send.
+ */
 
 export const testAction = data => ({ type: 'TEST' });
+
+export const testAction2 = data => ({ type: 'TEST', data: { foo: data } });
+
+export const testAction3 = (data) => {
+  return dispatch => {
+    /**
+     * Include your async code and dispatch events inside here.
+     */
+  }
+};
